4166829c659257ccac87c2975246ea2017d600a4
Ilfir <ilfir803@gmail.com>
2023-05-15 20:56:54 +0500

    изменен Title

---
+++ Homework/js/js.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+   $('.col-sm-offset-3, .col-sm-3, a[href="#sheldure"]').click(function() {
+      $('.modal').slideDown(1000);
+      $('.overlay').fadeIn(1000);
+   });
+   
+   $('.close').click(function() {
+      $('.modal').slideUp(1000);
+      $('.overlay').fadeOut(1000);
+   });
+   
+   // $('.col-sm-3').click(function() {
+   //    $('.modal').slideDown(1000);
+   //    $('.overlay').fadeIn(1000);
+   // });
+   
+   
+   // $('.main_mobmenu_field_list').click(function() {
+   //    $('.modal').slideDown(1000);
+   //    $('.overlay').fadeIn(1000);
+   // });
+ });
+
+
+
